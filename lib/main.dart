d23186e94126f73680ae6800846af6b999b2e582
ukyosa <dcm.ukyosakurai@gmail.com>
2023-11-28 15:45:32 +0900

    commit step2 end

--- lib/main.dart
+++ lib/main.dart
@@ -9,22 +9,53 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(title: const Text('Flutter layout demo')),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(40),
-          color: Colors.yellow,
-          child: const Column(
-            children: [
-              Text('Hello World!'),
-              Text('Hello World!'),
-              Text('Hello World!')
-            ],
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+          /*3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
+        ],
       ),
-    ));
+    );
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: Column(children: [
+          titleSection,
+        ]),
+      ),
+    );
   }
 }
