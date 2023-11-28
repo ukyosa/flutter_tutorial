cccbaf513487cf8c52a9cea29109c39247d66450
ukyosa <dcm.ukyosakurai@gmail.com>
2023-11-28 15:51:49 +0900

    commmit STEP3

--- lib/main.dart
+++ lib/main.dart
@@ -46,15 +46,46 @@ class MainApp extends StatelessWidget {
         ],
       ),
     );
+
+    Column buildButtonColumn(Color color, IconData icon, String label) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        buildButtonColumn(color, Icons.call, 'CALL'),
+        buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     return MaterialApp(
       title: 'Flutter layout demo',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Column(children: [
-          titleSection,
-        ]),
+        body: Column(children: [titleSection, buttonSection]),
       ),
     );
   }
